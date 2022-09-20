368f0419054c5276d61e7630f9a356967eeb0ae1
devcom2020 <kwp@tlen.pl>
2022-09-20 09:42:19 +0200

    wtorek 09:42

---
+++ petla.py
@@ -0,0 +1,28 @@
+while True:
+    print("""
+                  +++ MENU +++
+                1. przywitaj się
+                2. opuść program
+            +++ Program wersja 1.0 +++
+            
+    """)
+    wejscie = input("Wybierz 1-2:")
+    if wejscie == "1":
+        print("Witam w moim programie...")
+    elif wejscie == "2":
+        print("Do widzenia...")
+        break
+    else:
+        print("Błędny wybór! Spróbuj ponownie...")
+
+
+
+
+
+
+
+
+
+
+
+
