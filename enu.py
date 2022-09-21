3a47b6b76bd39dd32ab17e1073891ffa9f860341
devcom2020 <kwp@tlen.pl>
2022-09-21 13:35:51 +0200

    sroda 13:35

--- enu.py
+++ enu.py
@@ -1,7 +1,10 @@
-lista = ["a", "b", "c", 4, 5, 6]
+# lista = ["a", "b", "c", 4, 5, 6]
+#
+# for count, i in enumerate(lista, 1):
+#     print("Iteracja nr: ", count, "Wartość:", i)
 
-for count, i in enumerate(lista, 1):
-    print("Iteracja nr: ", count, "Wartość:", i)
+def test(*args, **kwargs):
+    pass
 
 
 def duzo(*liczby, **klucze):
@@ -9,11 +12,22 @@ def duzo(*liczby, **klucze):
     print(klucze)
 
 
-duzo()
-duzo(imie="Tomek")
-duzo(45, imie="Tomek")
-duzo(45)
+# duzo()
+# duzo(imie="Tomek")
+# duzo(45, imie="Tomek")
+# duzo(45)
 
 
+def maksimum(cyfra, *liczba):
+    print(cyfra)
+    if len(liczba) == 0:
+        return None
+    else:
+        liczba_max = liczba[0]
+        for n in liczba[1:]:
+            if n > liczba_max:
+                liczba_max = n
+        return liczba_max
 
 
+print(maksimum(55, 100, -500, 56))
